eee6d4f6373feb943769180c713cdbd69df380ce
jisu <jisuuoo96@gmail.com>
2023-02-14 14:34:39 +0900

    :white_check_mark: 회원 삭제 기능 테스트 작성

--- 스프링 DB (1)/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ 스프링 DB (1)/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -2,10 +2,11 @@ package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -29,6 +30,11 @@ class MemberRepositoryV0Test {
         repository.update(member.getMemberId(), 20000);
         Member updatedMember = repository.findById(member.getMemberId());
         assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        // delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() ->
+                repository.findById(member.getMemberId())).isInstanceOf(NoSuchElementException.class);
     }
 
 }
