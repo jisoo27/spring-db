6af4b610ca76fc0f7820773b16a33969236fc315
jisu <jisuuoo96@gmail.com>
2023-02-15 00:58:34 +0900

    :white_check_mark: 커넥션 풀을 이용하여 커넥션을 획득하는 테스트 작성

--- 스프링 DB (1)/src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
+++ 스프링 DB (1)/src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
@@ -1,11 +1,10 @@
 package hello.jdbc.repository;
 
+import com.zaxxer.hikari.HikariDataSource;
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
 import java.sql.SQLException;
 import java.util.NoSuchElementException;
 
@@ -20,12 +19,19 @@ class MemberRepositoryV1Test {
 
     @BeforeEach
     void beforeEach() {
-        // 기본 DriverManager - 항상 새로운 커넥션을 획득
-        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+//        // 기본 DriverManager - 항상 새로운 커넥션을 획득
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+
+        // 커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+
         repository = new MemberRepositoryV1(dataSource);
     }
 
-    
+
     @Test
     void crud() throws SQLException {
         // save
@@ -46,6 +52,12 @@ class MemberRepositoryV1Test {
         repository.delete(member.getMemberId());
         assertThatThrownBy(() ->
                 repository.findById(member.getMemberId())).isInstanceOf(NoSuchElementException.class);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
