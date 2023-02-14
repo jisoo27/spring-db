05b44a1b982d74c6afe623e39669e81a1b268734
jisu <jisuuoo96@gmail.com>
2023-02-15 00:32:50 +0900

    :white_check_mark: DataSource 예제 - 커넥션 풀 테스트

--- 스프링 DB (1)/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ 스프링 DB (1)/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,6 +1,6 @@
 package hello.jdbc.connection;
 
-import com.zaxxer.hikari.util.DriverDataSource;
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -34,6 +34,23 @@ public class ConnectionTest {
         Connection con2 = dataSource.getConnection();
         log.info("connection={}, class = {}" , con1, con1.getClass());
         log.info("connection={}, class = {}" , con2, con2.getClass());
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException { // spring 에서 JDBC 를 사용하면 자동으로 Hikari 가 import 된다.
+        // 커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        //풀이 다 찼을 경우 블락을 건다.
+
+        useDataSource(dataSource);
+        Thread.sleep(1000); // 커넥션 풀에 커넥션을 넣을때는 별도의 쓰레드에서 진행하기 때문에 더하는 과정이 보고싶다면 1초정도 sleep 을 주면 된다.
+
     }
 
 }
